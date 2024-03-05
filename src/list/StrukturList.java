9b6d9090c2c66f273b2e6bcb9fa204eab7ca54df
Arcaz22 <chandraarcychan@gmail.com>
2024-03-05 14:49:40 +0700

    fixing pertemuan 4

--- src/list/StrukturList.java
+++ src/list/StrukturList.java
@@ -24,42 +24,6 @@ public class StrukturList {
     }
   }
 
-  public void addHead(int data) {
-    Node newNode;
-    
-    newNode = new Node(data);
-    if (isEmpty()) {
-        HEAD = newNode;
-    } else {
-        newNode.setNext(HEAD);
-        HEAD = newNode;
-    }
-  }
-
-  public void addMid(int data, int position) {
-    Node previousNode = null, currentNode, newNode;
-    int i = 1;
-
-    newNode = new Node(data);
-    if (HEAD == null) {
-      HEAD = newNode;
-    } else {
-      currentNode = HEAD;
-      if (position == 1) {
-          newNode.next = currentNode;
-          HEAD = newNode;
-      } else {
-          while (currentNode != null && i < position) {
-              previousNode = currentNode;
-              currentNode = currentNode.next;
-              i++;
-          }
-          previousNode.next = newNode;
-          newNode.next = currentNode;
-      }
-    }
-  }
-
   public void removeHead() {
     if (isEmpty()) {
       System.out.println("List Kosong");
@@ -88,29 +52,36 @@ public class StrukturList {
   }
 
   public void removeMid(int x) {
-    if (HEAD != null) {
-      boolean ketemu = false;
-      int i = 1;
-      Node delNode = HEAD;
-      Node preNode = null;
-      while (delNode.next != null && !ketemu) {
-        if (delNode.data == x) {
-          ketemu = true;
-        } else {
-          preNode = delNode;
-          delNode = delNode.next;
-          i++;
-        }
-      }
-      if (ketemu) {
-        if (i == 1) {
-          HEAD = HEAD.next;
-        } else {
-          preNode.next = delNode.next;
-          delNode = null;
-        }
+    Node previousNode = null, 
+    currentNode = HEAD;
+    boolean found = false;
+
+    if (isEmpty()) {
+      System.out.println("List Kosong");
+      return;
+    }
+  
+    while (currentNode != null && !found) {
+      if (currentNode.getData() == x) {
+        found = true;
+      } else {
+        previousNode = currentNode;
+        currentNode = currentNode.getNext();
       }
     }
+
+    if (!found) {
+      System.out.println("Node dengan nilai " + x + " tidak ditemukan!");
+      return;
+    }
+  
+    if (previousNode == null) {
+      HEAD = HEAD.getNext();
+    } else {
+      previousNode.setNext(currentNode.getNext());
+    }
+  
+    currentNode.setNext(null);
   }
 
   public void display() {
